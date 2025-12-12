2fa4fdeba61f296925ccdeae0bfaab1e20d29a6d
rayfrance <rayanethaisglfrance@gmail.com>
2025-12-12 15:41:59 -0300

    ANX-02: [feat] configure sql server context and dependency injection

--- AutoNexus.Web/Program.cs
+++ AutoNexus.Web/Program.cs
@@ -1,4 +1,12 @@
+using AutoNexus.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -9,7 +17,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
