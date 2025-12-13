474dd3ab26190166d332669f79d82d9f52ea241d
rayfrance <rayanethaisglfrance@gmail.com>
2025-12-13 19:22:27 -0300

    ANX-03: [refactor] centralize constants and apply DRY in DbInitializer

---
+++ AutoNexus.Domain/Constants.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AutoNexus.Domain
+{
+    public static class Constants
+    {
+        #region Roles
+        public const string AdminRole = "Admin";
+        public const string SellerRole = "Vendedor";
+
+        public static readonly IReadOnlyCollection<string> AllRoles = new ReadOnlyCollection<string>(new[]
+        {
+            AdminRole,
+            SellerRole
+        });
+        #endregion
+
+        #region Seed Users - Admin
+        public const string AdminEmail = "admin@autonexus.com";
+        public const string AdminPassword = "Admin@123";
+        #endregion
+
+        #region Seed Users - Vendedor
+        public const string SellerEmail = "vendedor@autonexus.com";
+        public const string SellerPassword = "Vendedor@123";
+        #endregion
+    }
+}
--- AutoNexus.Infrastructure/Data/DbInitializer.cs.cs
+++ AutoNexus.Infrastructure/Data/DbInitializer.cs.cs
@@ -1,52 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using AutoNexus.Domain;
 
 namespace AutoNexus.Infrastructure.Data
 {
     public static class DbInitializer
     {
-        private const string ADMIN_EMAIL = "admin@autonexus.com";
-        private const string SELLER_EMAIL = "vendedor@autonexus.com";
-        private const string SELLER_PASSWORD = "Vendedor@123";
-        private const string ADMIN_PASSWORD = "Admin@123";
-
         public static async Task Initialize(IServiceProvider serviceProvider)
         {
-            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
-            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            UserManager<IdentityUser> userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            RoleManager<IdentityRole> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
             await CreateRoles(roleManager);
-
             await CreateAdminUser(userManager);
-
             await CreateSellerUser(userManager);
         }
-
-        private static async Task CreateSellerUser(UserManager<IdentityUser> userManager)
-        {
-            if (await userManager.FindByEmailAsync(SELLER_EMAIL)! == null)
-                return;
-
-            IdentityUser seller = new() { UserName = SELLER_EMAIL, Email = SELLER_EMAIL, EmailConfirmed = true };
-            await userManager.CreateAsync(seller, SELLER_PASSWORD);
-            await userManager.AddToRoleAsync(seller, "Vendedor");
-        }
-
-        private static async Task CreateAdminUser(UserManager<IdentityUser> userManager)
-        {
-            if (await userManager.FindByEmailAsync(ADMIN_EMAIL) != null)
-                return;
-
-            IdentityUser admin = new() { UserName = ADMIN_EMAIL, Email = ADMIN_EMAIL, EmailConfirmed = true };
-            await userManager.CreateAsync(admin, ADMIN_PASSWORD);
-            await userManager.AddToRoleAsync(admin, "Admin");
-            
-        }
-
         private static async Task CreateRoles(RoleManager<IdentityRole> roleManager)
         {
-            string[] roleNames = { "Admin", "Vendedor" };
-            foreach (var roleName in roleNames)
+            foreach (var roleName in Constants.AllRoles)
             {
                 if (!await roleManager.RoleExistsAsync(roleName))
                 {
@@ -54,5 +25,35 @@ namespace AutoNexus.Infrastructure.Data
                 }
             }
         }
+        private static async Task CreateSellerUser(UserManager<IdentityUser> userManager)
+        {
+            if (await userManager.FindByEmailAsync(Constants.SellerEmail) != null)
+                return;
+
+            IdentityUser sellerUser = CreateUser(Constants.SellerEmail);
+
+            await userManager.CreateAsync(sellerUser, Constants.SellerPassword);
+            await userManager.AddToRoleAsync(sellerUser, Constants.SellerRole);
+        }
+        private static async Task CreateAdminUser(UserManager<IdentityUser> userManager)
+        {
+            if (await userManager.FindByEmailAsync(Constants.AdminEmail) != null)
+                return;
+
+            IdentityUser adminUser = CreateUser(Constants.AdminEmail);
+
+            await userManager.CreateAsync(adminUser, Constants.AdminPassword);
+            await userManager.AddToRoleAsync(adminUser, Constants.AdminRole);
+        }
+        private static IdentityUser CreateUser(string userEmail)
+        {
+            return new()
+            {
+                UserName = UserEmail,
+                Email = UserEmail,
+                EmailConfirmed = true
+            };
+        }
+
     }
 }
