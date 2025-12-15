e1f89d76bec6edbef2509b03defe3adf283970e8
rayfrance <rayanethaisglfrance@gmail.com>
2025-12-14 23:20:13 -0300

    ANX-06: create configuration classes for each entity

---
+++ AutoNexus.Infrastructure/Persistence/Configuration/ClientConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AutoNexus.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoNexus.Infrastructure.Persistence.Configurations
+{
+    public class ClientConfiguration : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.Property(c => c.Name).IsRequired().HasMaxLength(200);
+            builder.Property(c => c.Email).IsRequired().HasMaxLength(150);
+            builder.Property(c => c.CPF).IsRequired().HasMaxLength(14); // 111.222.333-44
+            builder.Property(c => c.Phone).HasMaxLength(20);
+            builder.Property(c => c.ZipCode).HasMaxLength(10);
+        }
+    }
+}
---
+++ AutoNexus.Infrastructure/Persistence/Configuration/SaleConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using AutoNexus.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoNexus.Infrastructure.Persistence.Configurations
+{
+    public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.Property(s => s.ProtocolNumber).IsRequired().HasMaxLength(50);
+
+            builder.Property(s => s.FinalPrice)
+                .HasColumnType("decimal(18,2)");
+
+            builder.HasOne(s => s.Client)
+                .WithMany(c => c.Purchases)
+                .HasForeignKey(s => s.ClientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(s => s.Vehicle)
+                .WithMany()
+                .HasForeignKey(s => s.VehicleId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
